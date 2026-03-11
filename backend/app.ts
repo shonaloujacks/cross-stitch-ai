658aff7590c879a9cf97d950adf9de412f40d67e
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-03-11 15:56:21 +0000

    generate endpoint set up

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,12 @@
 import express from 'express';
+import generateRouter from './routes/generate'
+
 const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173']}));
+
+app.use('/api/generate', generateRouter);
 
 export default app;
---
+++ backend/routes/generate.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import Anthropic from '@anthropic-ai/sdk';
+
+
+const router = express.Router();
+const client = new Anthropic({
+  apiKey: process.env['ANTHROPIC_API_KEY']
+});
+
+router.post('/', async(req, res) => {
+
+  const { prompt, height, width } = req.body
+
+  const message = await client.messages.create({
+    max_tokens: 2048,
+    model: 'claude-sonnet-4-6',
+    system: 'Always respond with JSON in this exact structure: {title, width, height, palette, grid}. Never include markdown. The grid should be a 2D array of palette indices where -1 means empty and each number refers to a palette index. Each palette object should have the fields: "color", "symbol" and "name". The "color" field should use hex format ie. #f5c618. The palette should have no more than 10 colors. The title should be a short description based on the user`s prompt.',
+    messages: [{ role: 'user', content: `Create a cross stich pattern of ${prompt}. The grid should be ${width}x${height}.`}]
+  });
+
+  const contentBlock = message.content[0]
+  if (!contentBlock || contentBlock.type !== 'text'){
+    res.status(500).json({error: 'Unexpected response from Claude'});
+    return; 
+  }
+  const parsedPattern = JSON.parse(contentBlock.text)
+  res.json(parsedPattern)
+
+});
+
+export default router;
