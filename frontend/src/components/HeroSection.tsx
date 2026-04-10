b9086122b5eb8b67cec78806f37d050ee97dba73
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-04-10 17:23:34 +0100

    hero responsiveness adjusted for tablets

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -9,19 +9,19 @@ import heart from '../assets/images/heart-surrounded-by-wreath-flowers.jpg';
 
 
 const images = [
-  { src: heart,   top: '6%',  left: '0%',  mobileLeft: '0%',  rotate: -6, hideOnMobile: false},
-  { src: pizza,   top: '13%', left: '15%', mobileLeft: '25%', rotate: 3,  hideOnMobile: false},
-  { src: cat,     top: '5%',  left: '30%', mobileLeft: '50%', rotate: -4, hideOnMobile: false},
-  { src: avocado, top: '12%', left: '43%', mobileLeft: '75%', rotate: 5,  hideOnMobile: false},
-  { src: dog,     top: '4%',  left: '58%', mobileLeft: '60%', rotate: -3, hideOnMobile: true },
-  { src: chilli,  top: '13%', left: '70%', mobileLeft: '72%', rotate: 4,  hideOnMobile: true },
-  { src: beach,   top: '5%',  left: '86%', mobileLeft: '88%', rotate: -5, hideOnMobile: true },
+  { src: heart,   top: '6%',  left: '0%',  mobileLeft: '0%',  rotate: -6, hideOnMobile: false, hideOnTablet: false},
+  { src: pizza,   top: '13%', left: '15%', mobileLeft: '25%', rotate: 3,  hideOnMobile: false, hideOnTablet: false},
+  { src: cat,     top: '5%',  left: '30%', mobileLeft: '50%', rotate: -4, hideOnMobile: false, hideOnTablet: false},
+  { src: avocado, top: '12%', left: '43%', mobileLeft: '75%', rotate: 5,  hideOnMobile: true, hideOnTablet: false},
+  { src: dog,     top: '4%',  left: '58%', mobileLeft: '60%', rotate: -3, hideOnMobile: true, hideOnTablet: true },
+  { src: chilli,  top: '13%', left: '70%', mobileLeft: '72%', rotate: 4,  hideOnMobile: true, hideOnTablet: true },
+  { src: beach,   top: '5%',  left: '86%', mobileLeft: '88%', rotate: -5, hideOnMobile: true, hideOnTablet: true },
 
 ];
 
 const HeroSection = () => {
   return (
-    <Box sx={{ position: 'relative', height: 320, maxWidth: 1500, mx: 'auto', mt: 1.5 }}>
+    <Box sx={{ position: 'relative', height: 320, maxWidth: 1700, mx: 'auto', mt: 1.5 }}>
       {images.map((img, i) => (
         <Paper
           key={i}
@@ -32,7 +32,7 @@ const HeroSection = () => {
             left: { xs: img.mobileLeft, md: img.left },
             transform: `rotate(${img.rotate}deg)`,
             lineHeight: 0,
-            display: img.hideOnMobile ? { xs: 'none', md: 'block' } : 'block',
+            display: img.hideOnTablet ? { xs: 'none', md: 'block'} : img.hideOnMobile ? { xs: 'none', sm: 'block' } : 'block'
           }}
         >
           <img src={img.src} style={{ height: 250, display: 'block' }} />
