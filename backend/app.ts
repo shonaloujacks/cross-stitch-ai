93922deca86d5da2f4870c5296916ee182b701aa
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-04-10 16:26:55 +0100

    new frontend url added to cors

--- backend/app.ts
+++ backend/app.ts
@@ -15,7 +15,7 @@ const generateLimiter = rateLimit({
 })
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173']}));
+app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://cross-stitch-ai.pages.dev']}));
 
 app.use('/api/generate', generateRouter, generateLimiter);
 
