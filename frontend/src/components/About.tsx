3f59f852f64ab0977d62db10a088294e243fed25
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-04-13 16:27:32 +0100

    margins tweaked

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
 
   return (
   
-  <Paper elevation={5} sx={{ p: 2, mt: 4, maxWidth: 800, ml: 'auto', mr: 'auto', mb: 2, display: 'flex', textAlign: 'center'}}>
+  <Paper elevation={5} sx={{ p: 2, mt: 1, maxWidth: 800, ml: 'auto', mr: 'auto', mb: 2, display: 'flex', textAlign: 'center'}}>
     <Box sx={{ border: '3px dashed #88c4a8d5', p: {xs: 1, sm: 2, md: 4, lg: 5}}}>
     <Typography sx={{color: '#444', mx: 5, mb: 2, mt: 2, letterSpacing: 1 }}>
       CrossStitchAI is a pattern generation tool powered by OpenAI, built to give crafters a quick way to produce basic, small-scale designs. It is not — and does not seek to be — a replacement for the unrivalled skill and artistry of human pattern makers.
