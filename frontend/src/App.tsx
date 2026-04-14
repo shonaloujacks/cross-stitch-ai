45d7cdb3f3f41ff8ad153a876ca77f341eff2da8
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-04-14 09:24:46 +0100

    clickaway listener added to mobile menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';   
-import { Box, CircularProgress, Typography, useMediaQuery, Paper } from "@mui/material";
+import { Box, CircularProgress, ClickAwayListener, Typography, useMediaQuery, Paper } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import type { Pattern } from './types';
 import PromptForm from './components/PromptForm';
@@ -53,13 +53,16 @@ const App = () => {
           </Box>
         {isMobile && <MenuIcon onClick={toggleMenu} sx={{color: '#88c4a8d5'}}/>} 
         {isMobile && menuOpen ? 
+        <ClickAwayListener onClickAway={() => setMenuOpen(false)}>
          <Paper elevation={12} sx={{display: 'flex', flexDirection: 'column', position:'absolute', top: '100%', right: 0, backgroundColor: 'white', padding: 2, letterSpacing: 4, alignItems: 'center'}}> 
           <Link to="/" onClick={() => setMenuOpen(false)} style={{ color: '#333', padding: 4 }}>create pattern</Link>
           <span style={{ color: '#88c4a8d5', fontSize: 18 }}>✕</span>    
           <Link to="/guidance" onClick={() => setMenuOpen(false)} style={{ color: '#333', padding: 4 }}>prompt guidance</Link>
           <span style={{ color: '#88c4a8d5', fontSize: 18 }}>✕</span>    
           <Link to="/about" onClick={() => setMenuOpen(false)} style={{ color: '#333', padding: 4 }}>about</Link> 
-          </Paper> : null }
+          </Paper>
+          </ClickAwayListener>
+           : null }
         {!isMobile && <Box sx={{ display: 'flex', gap: 10, letterSpacing: 4, justifyContent: 'center', flexGrow: 1}}>     
           <Link to="/" style={{ textDecoration: 'none', color: '#333', fontSize: 18}}>create pattern</Link>
           <span style={{ color: '#88c4a8d5', fontSize: 24 }}>✕</span>     
